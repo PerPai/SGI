6f89cbd4c7c2372414346fe92ea03c2273036c75
PerPai <116335452+PerPai@users.noreply.github.com>
2024-06-28 19:49:36 -0600

    first commit

---
+++ backend/src/controllers/rol.js
@@ -0,0 +1,21 @@
+import { pool } from "../database.js";
+import bcrypt from "bcrypt";
+import InternalError from "../errors/handleError.js";
+
+export const GetRolesAsignados = async (req, res) => {
+    try {
+      const { CT_CEDULA } = req.query;
+  
+      const [rows] = await pool.query(
+        'CALL GetRolesAsignados(?)',
+        [CT_CEDULA]
+      );
+  
+      if (rows[0].length <= 0) {
+        return res.status(404).json({ message: 0 })
+      }
+      return res.json(rows[0])
+    } catch (error) {
+      return error;
+    }
+  } 
